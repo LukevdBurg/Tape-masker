5f21c62a1673aa553ab4ce3915b16c1850e165bf
LukevdBurg <46902130+LukevdBurg@users.noreply.github.com>
2020-01-15 14:25:31 +0100

    Add files via upload

--- GUI/GUI-Resize.py
+++ GUI/GUI-Resize.py
@@ -8,46 +8,61 @@ class MyApp:
 
         self.myParent.attributes('-fullscreen', True)
         self.myParent.title("Masking Robot KLM")
+        self.myParent['bg'] = '#00A1E4'
         self.myParent.grid_columnconfigure(0, weight=1)
         self.myParent.grid_rowconfigure(0, weight=1)
 
-        self.button_frame = Frame(self.myParent, bg='grey')
-        self.button_frame.grid(row=0, column=0, sticky=NSEW)
+        self.logo_frame = Frame(self.myParent, bg='#00A1E4')
+        self.logo_frame.grid(row=0, column=0, sticky=NSEW)
 
-        self.progress_frame = Frame(self.myParent, bg='grey')
-        self.progress_frame.grid(row=1, column=0, sticky=NSEW)
+        self.button_frame = Frame(self.myParent, bg='#00A1E4')
+        self.button_frame.grid(row=1, column=0, sticky=NSEW)
 
-        self.coord_frame = Frame(self.myParent, bg='grey')
-        self.coord_frame.grid(row=2, column=0, sticky=NSEW)
+        self.progress_frame = Frame(self.myParent, bg='#00A1E4')
+        self.progress_frame.grid(row=2, column=0, sticky=NSEW)
 
-        self.button_frame.grid_rowconfigure(0, weight=1)
+        self.coord_frame = Frame(self.myParent, bg='#00A1E4')
+        self.coord_frame.grid(row=3, column=0, sticky=NSEW)
+
+        self.footer_frame = Frame(self.myParent, bg='#00A1E4')
+        self.footer_frame.grid(row=4, column=0, sticky=NSEW)
+
+        # self.logo_frame.grid_rowconfigure(0, weight=1)
+        self.logo_frame.grid_columnconfigure(0, weight=1)
+        self.logo_frame.grid_columnconfigure(1, weight=1)
+
+        self.button_frame.grid_rowconfigure(1, weight=1)
         self.button_frame.grid_columnconfigure(0, weight=1)
         self.button_frame.grid_columnconfigure(1, weight=1)
         self.button_frame.grid_columnconfigure(2, weight=1)
         self.button_frame.grid_columnconfigure(3, weight=1)
 
-        self.progress_frame.grid_rowconfigure(1, weight=1)
+        # self.progress_frame.grid_rowconfigure(2, weight=1)
         self.progress_frame.grid_columnconfigure(0, weight=1)
 
-        self.coord_frame.grid_rowconfigure(2, weight=1)
-        self.coord_frame.grid_rowconfigure(3, weight=1)
-        self.coord_frame.grid_rowconfigure(4, weight=1)
+        # self.coord_frame.grid_rowconfigure(3, weight=1)
+        # self.coord_frame.grid_rowconfigure(4, weight=1)
+        # self.coord_frame.grid_rowconfigure(5, weight=1)
         self.coord_frame.grid_columnconfigure(0, weight=1)
         self.coord_frame.grid_columnconfigure(1, weight=1)
         self.coord_frame.grid_columnconfigure(2, weight=1)
         self.coord_frame.grid_columnconfigure(3, weight=1)
 
-        self.start_button = Button(self.button_frame, command=self.buttonstartclick, padx=5, pady=5, text='Start',
-                                   background='green')
+        # self.footer_frame.grid_rowconfigure(6, weight=1)
+        self.footer_frame.grid_columnconfigure(0, weight=1)
+
+        head_label = Label(self.logo_frame, text='KLM TAPE MASKING ROBOT', padx=25, pady=25)
+        head_label.grid(row=0, column=0, sticky='EWNS')
+
+        self.start_button = Button(self.button_frame, command=self.buttonstartclick, padx=5, pady=5, text='Start')
         self.start_button.grid(row=0, column=0, pady=25, padx=25, sticky='EWNS')
         self.start_button.bind("<Return>", self.buttonstartclick_a)
 
-        self.stop_button = Button(self.button_frame, command=self.buttonstopclick, padx=5, pady=5, text='Stop',
-                                  background='red')
+        self.stop_button = Button(self.button_frame, command=self.buttonstopclick, padx=5, pady=5, text='Stop')
         self.stop_button.grid(row=0, column=1, pady=25, padx=25, sticky='EWNS')
         self.stop_button.bind("<Return>", self.buttonstopclick_a)
 
-        self.reset_button = Button(self.button_frame, padx=5, pady=5, text='Reset', background='red')
+        self.reset_button = Button(self.button_frame, padx=5, pady=5, text='Reset')
         self.reset_button.grid(row=0, column=2, pady=25, padx=25, sticky='EWNS')
 
         self.exit_button = Button(self.button_frame, command=self.buttonexitclick, padx=5, pady=5, text='Exit')
@@ -57,13 +72,13 @@ class MyApp:
         self.progress = ttk.Progressbar(self.progress_frame, mode='determinate', length=300)
         self.progress.grid(column=0, columnspan=4, row=2, padx=5, pady=25, sticky='EWNS')
 
-        Label(self.coord_frame, text="X", background='grey').grid(column=0, row=0, sticky='EWNS', padx=5, pady=15)
-        Label(self.coord_frame, text="Y", background='grey').grid(column=0, row=1, sticky='EWNS', padx=5, pady=15)
-        Label(self.coord_frame, text="Z", background='grey').grid(column=0, row=2, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="X", background='#00A1E4').grid(column=0, row=0, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="Y", background='#00A1E4').grid(column=0, row=1, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="Z", background='#00A1E4').grid(column=0, row=2, sticky='EWNS', padx=5, pady=15)
 
-        Label(self.coord_frame, text="RX", background='grey').grid(column=2, row=0, sticky='EWNS', padx=5, pady=15)
-        Label(self.coord_frame, text="RY", background='grey').grid(column=2, row=1, sticky='EWNS', padx=5, pady=15)
-        Label(self.coord_frame, text="RZ", background='grey').grid(column=2, row=2, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="RX", background='#00A1E4').grid(column=2, row=0, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="RY", background='#00A1E4').grid(column=2, row=1, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="RZ", background='#00A1E4').grid(column=2, row=2, sticky='EWNS', padx=5, pady=15)
 
         myX = "5,55"
         myY = "6,66"
@@ -87,18 +102,16 @@ class MyApp:
         Label(self.coord_frame, text=myRZ, background="white", relief=SUNKEN, padx=20, pady=20).grid(column=3, row=2,
                                                                                                      sticky='EWNS')
 
+        Label(self.footer_frame, text="", pady=20, background="#00A1E4").grid(column=0, row=0, sticky='EWNS')
+
     def buttonstartclick(self):
-        if self.start_button["background"] == 'green':
-            self.start_button["background"] = "yellow"
-            self.start_button.configure(state="disabled")
+        self.start_button.configure(state="disabled")
 
     def buttonstartclick_a(self):
         self.buttonstartclick()
 
     def buttonstopclick(self):
-        if self.start_button["background"] == "yellow":
-            self.start_button["background"] = "green"
-            self.start_button.configure(state="normal")
+        self.start_button.configure(state="normal")
 
     def buttonstopclick_a(self):
         self.buttonstopclick()
