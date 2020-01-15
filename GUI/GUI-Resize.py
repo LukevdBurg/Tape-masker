d17d5b21a0532c6a1ff16d0febef08f4584faeca
LukevdBurg <46902130+LukevdBurg@users.noreply.github.com>
2020-01-15 12:15:12 +0100

    Add files via upload

--- GUI/GUI-Resize.py
+++ GUI/GUI-Resize.py
@@ -1,69 +1,115 @@
-from tkinter import *
 import tkinter.ttk as ttk
+from tkinter import *
+
+
+class MyApp:
+    def __init__(self, parent):
+        self.myParent = parent
+
+        self.myParent.attributes('-fullscreen', True)
+        self.myParent.title("Masking Robot KLM")
+        self.myParent.grid_columnconfigure(0, weight=1)
+        self.myParent.grid_rowconfigure(0, weight=1)
+
+        self.button_frame = Frame(self.myParent, bg='grey')
+        self.button_frame.grid(row=0, column=0, sticky=NSEW)
+
+        self.progress_frame = Frame(self.myParent, bg='grey')
+        self.progress_frame.grid(row=1, column=0, sticky=NSEW)
+
+        self.coord_frame = Frame(self.myParent, bg='grey')
+        self.coord_frame.grid(row=2, column=0, sticky=NSEW)
+
+        self.button_frame.grid_rowconfigure(0, weight=1)
+        self.button_frame.grid_columnconfigure(0, weight=1)
+        self.button_frame.grid_columnconfigure(1, weight=1)
+        self.button_frame.grid_columnconfigure(2, weight=1)
+        self.button_frame.grid_columnconfigure(3, weight=1)
+
+        self.progress_frame.grid_rowconfigure(1, weight=1)
+        self.progress_frame.grid_columnconfigure(0, weight=1)
+
+        self.coord_frame.grid_rowconfigure(2, weight=1)
+        self.coord_frame.grid_rowconfigure(3, weight=1)
+        self.coord_frame.grid_rowconfigure(4, weight=1)
+        self.coord_frame.grid_columnconfigure(0, weight=1)
+        self.coord_frame.grid_columnconfigure(1, weight=1)
+        self.coord_frame.grid_columnconfigure(2, weight=1)
+        self.coord_frame.grid_columnconfigure(3, weight=1)
+
+        self.start_button = Button(self.button_frame, command=self.buttonstartclick, padx=5, pady=5, text='Start',
+                                   background='green')
+        self.start_button.grid(row=0, column=0, pady=25, padx=25, sticky='EWNS')
+        self.start_button.bind("<Return>", self.buttonstartclick_a)
+
+        self.stop_button = Button(self.button_frame, command=self.buttonstopclick, padx=5, pady=5, text='Stop',
+                                  background='red')
+        self.stop_button.grid(row=0, column=1, pady=25, padx=25, sticky='EWNS')
+        self.stop_button.bind("<Return>", self.buttonstopclick_a)
+
+        self.reset_button = Button(self.button_frame, padx=5, pady=5, text='Reset', background='red')
+        self.reset_button.grid(row=0, column=2, pady=25, padx=25, sticky='EWNS')
+
+        self.exit_button = Button(self.button_frame, command=self.buttonexitclick, padx=5, pady=5, text='Exit')
+        self.exit_button.grid(row=0, column=3, pady=25, padx=25, sticky='EWNS')
+        self.exit_button.bind("<Return>", self.buttonexitclick_a)
+
+        self.progress = ttk.Progressbar(self.progress_frame, mode='determinate', length=300)
+        self.progress.grid(column=0, columnspan=4, row=2, padx=5, pady=25, sticky='EWNS')
+
+        Label(self.coord_frame, text="X", background='grey').grid(column=0, row=0, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="Y", background='grey').grid(column=0, row=1, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="Z", background='grey').grid(column=0, row=2, sticky='EWNS', padx=5, pady=15)
+
+        Label(self.coord_frame, text="RX", background='grey').grid(column=2, row=0, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="RY", background='grey').grid(column=2, row=1, sticky='EWNS', padx=5, pady=15)
+        Label(self.coord_frame, text="RZ", background='grey').grid(column=2, row=2, sticky='EWNS', padx=5, pady=15)
+
+        myX = "5,55"
+        myY = "6,66"
+        myZ = "7,77"
+
+        Label(self.coord_frame, text=myX, background="white", relief=SUNKEN, padx=20, pady=20).grid(column=1, row=0,
+                                                                                                    sticky='EWNS')
+        Label(self.coord_frame, text=myY, background="white", relief=SUNKEN, padx=20, pady=20).grid(column=1, row=1,
+                                                                                                    sticky='EWNS')
+        Label(self.coord_frame, text=myZ, background="white", relief=SUNKEN, padx=20, pady=20).grid(column=1, row=2,
+                                                                                                    sticky='EWNS')
+
+        myRX = "8,88"
+        myRY = "9,99"
+        myRZ = "1,11"
+
+        Label(self.coord_frame, text=myRX, background="white", relief=SUNKEN, padx=20, pady=20).grid(column=3, row=0,
+                                                                                                     sticky='EWNS')
+        Label(self.coord_frame, text=myRY, background="white", relief=SUNKEN, padx=20, pady=20).grid(column=3, row=1,
+                                                                                                     sticky='EWNS')
+        Label(self.coord_frame, text=myRZ, background="white", relief=SUNKEN, padx=20, pady=20).grid(column=3, row=2,
+                                                                                                     sticky='EWNS')
+
+    def buttonstartclick(self):
+        if self.start_button["background"] == 'green':
+            self.start_button["background"] = "yellow"
+            self.start_button.configure(state="disabled")
+
+    def buttonstartclick_a(self):
+        self.buttonstartclick()
+
+    def buttonstopclick(self):
+        if self.start_button["background"] == "yellow":
+            self.start_button["background"] = "green"
+            self.start_button.configure(state="normal")
+
+    def buttonstopclick_a(self):
+        self.buttonstopclick()
+
+    def buttonexitclick(self):
+        root.destroy()
+
+    def buttonexitclick_a(self, event):
+        self.buttonexitclick()
+
 
 root = Tk()
-root.grid_columnconfigure(0, weight=1)
-root.grid_rowconfigure(0, weight=1)
-keyboard = Frame(root, padx=5, pady=5, bg="grey")
-keyboard.grid(row=0, column=0, sticky=NSEW)
-
-keyboard.grid_rowconfigure(0, weight=1)
-keyboard.grid_columnconfigure(0, weight=1)
-keyboard.grid_columnconfigure(1, weight=1)
-keyboard.grid_columnconfigure(2, weight=1)
-keyboard.grid_columnconfigure(3, weight=1)
-
-
-def buttonExitclick():
-    root.destroy()
-
-
-def buttonStartclick():
-    if start_button["background"] == 'green':
-        start_button["background"] = "yellow"
-        start_button.configure(state="disabled")
-
-
-def buttonStopclick():
-    if start_button["background"] == "yellow":
-        start_button["background"] = "green"
-        start_button.configure(state="normal")
-
-
-start_button = Button(keyboard, command=buttonStartclick, padx=5, pady=5, text='Start', background='green')
-start_button.grid(row=0, column=0, pady=25 , padx=25,sticky='EWNS')
-stop_button = Button(keyboard, command=buttonStopclick, padx=5, pady=5, text='Stop', background='red')
-stop_button.grid(row=0, column=1, pady=25, padx=25, sticky='EWNS')
-reset_button = Button(keyboard, padx=5, pady=5, text='Reset', background='red')
-reset_button.grid(row=0, column=2, pady=25, padx=25, sticky='EWNS')
-exit_button = Button(keyboard, command=buttonExitclick, padx=5, pady=5, text='Exit')
-exit_button.grid(row=0, column=3, pady=25, padx=25, sticky='EWNS')
-
-progress = ttk.Progressbar(keyboard, mode='determinate', length=300)
-progress.grid(column=0, columnspan=4, row=2, padx=5, pady=25, sticky='EWNS')
-
-Label(keyboard, text="X", background= 'grey').grid(column=0, row=3, sticky='EWNS', padx=5, pady=15)
-Label(keyboard, text="Y", background= 'grey').grid(column=0, row=4, sticky='EWNS', padx=5, pady=15)
-Label(keyboard, text="Z", background= 'grey').grid(column=0, row=5, sticky='EWNS', padx=5, pady=15)
-
-Label(keyboard, text="RX", background= 'grey').grid(column=2, row=3, sticky='EWNS', padx=5, pady=15)
-Label(keyboard, text="RY", background= 'grey').grid(column=2, row=4, sticky='EWNS', padx=5, pady=15)
-Label(keyboard, text="RZ", background= 'grey').grid(column=2, row=5, sticky='EWNS', padx=5, pady=15)
-
-myX = "5,55"
-myY = "6,66"
-myZ = "7,77"
-
-Label(keyboard, text=myX, background="white", relief=SUNKEN, padx=20).grid(column=1, row=3, sticky='EWNS')
-Label(keyboard, text=myY, background="white", relief=SUNKEN, padx=20).grid(column=1, row=4, sticky='EWNS')
-Label(keyboard, text=myZ, background="white", relief=SUNKEN, padx=20).grid(column=1, row=5, sticky='EWNS')
-
-myRX = "8,88"
-myRY = "9,99"
-myRZ = "1,11"
-Label(keyboard, text=myRX, background="white", relief=SUNKEN, padx=20).grid(column=3, row=3, sticky='EWNS')
-Label(keyboard, text=myRY, background="white", relief=SUNKEN, padx=20).grid(column=3, row=4, sticky='EWNS')
-Label(keyboard, text=myRZ, background="white", relief=SUNKEN, padx=20).grid(column=3, row=5, sticky='EWNS')
-
-root.attributes('-fullscreen', True)
+myapp = MyApp(root)
 root.mainloop()
