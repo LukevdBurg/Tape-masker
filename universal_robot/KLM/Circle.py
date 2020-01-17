559ba28211e47e628fa0e4554ddd0e0dd0020bc5
LukevdBurg <46902130+LukevdBurg@users.noreply.github.com>
2020-01-17 10:33:46 +0100

    Introduced Class
    Needs some testing

--- universal_robot/KLM/Circle.py
+++ universal_robot/KLM/Circle.py
@@ -11,110 +11,115 @@ import time
 import matplotlib.patches as mpatches
 
 from math import pi, cos, sin, acos
-import math 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import lidar as lidar
 
 
+class MyRobot(object):
+    pass
+
 def startup():
-    #Start position with all joints within limits
+    # Start position with all joints within limits
     print("Going to startup pose! \n")
     rob.movej([0.283, -0.416, -2.305, 1.152, 1.571, -0.283], acc=a, vel=v)
-    
-    
+
+
 def calibrateCenter():
     # Calibrate to the center of the rotor
     print("Calibrating Center!")
     # Rotate EOAT 90 degrees for LIDAR
     pose = rob.getl()
-    rob.movel([pose[0], pose[1], pose[2], -1.21, 1.21, -1.21], acc=a, vel=v/2)
-    pose = rob.getl() 
+    rob.movel([pose[0], pose[1], pose[2], -1.21, 1.21, -1.21], acc=a, vel=v / 2)
+    pose = rob.getl()
     lidarCheck = False
-    
+
     # Move 50mm forward till LIDAR is within motor. 
     for i in range(20):
-#        if i == 5: lidarCheck = True
+        #        if i == 5: lidarCheck = True
         lidarCheck = lidar.find_circle()
-        if (lidarCheck== True): break
+        if (lidarCheck == True): break
         pose[2] += 0.05
-        rob.movel(pose, acc=a/2, vel=v)
-    
+        rob.movel(pose, acc=a / 2, vel=v)
+
     # Move LIDAR to center of motor
     deltaX, deltaY = lidar.find_middle()
     pose[0] += deltaX
     pose[1] += deltaY
-    rob.movel(pose, acc=a, vel=v) 
+    rob.movel(pose, acc=a, vel=v)
 
-    #Second measurement with the lidar to check. 
+    # Second measurement with the lidar to check.
     deltaX, deltaY = lidar.find_middle()
-    pose[0] += deltaX + 0.061 # deltaX + offset of lidar
-    pose[1] += deltaY + 0.0185 # deltaY + offset of lidar
+    pose[0] += deltaX  # deltaX + offset of lidar
+    pose[1] += deltaY  # deltaY + offset of lidar
 
-    #rotate so that tip EOAT is in LIDAR pose    
+    # rotate so that tip EOAT is in LIDAR pose
     pose = [pose[0], pose[1], pose[2], 0, 0, -1.57]
     rob.movel(pose, acc=a, vel=v)
     print("Done calibrating center! \n")
-   
+
 
 def tapeStation():
     # Go to tapestation and grab tape
     print("Going to tapestation!")
     v = 0.2
     a = 0.2
-    
-    #Hardcoded poses of the station
-#    stationPose = [-0.161,  0.153,  0.912,  1.027,  1.392, -1.026]
-    stationJPose = [-1.488 , -0.328 , -1.828 , -1.041 , 1.746 , 1.519] #starting J pose
-    grabTapePose = [-0.135 , 0.108 , 0.912 , 1.027 , 1.392 , -1.026] #Grabbing pose
-    forwardTapePose = [-0.135 , 0.03 , 0.912 , 1.16 , 1.322 , -1.146] #move forward and flatten
-    
+
+    # Hardcoded poses of the station
+    #    stationPose = [-0.161,  0.153,  0.912,  1.027,  1.392, -1.026]
+    stationJPose = [-1.488, -0.328, -1.828, -1.041, 1.746, 1.519]  # starting J pose
+    grabTapePose = [-0.135, 0.108, 0.912, 1.027, 1.392, -1.026]  # Grabbing pose
+    forwardTapePose = [-0.135, 0.03, 0.912, 1.16, 1.322, -1.146]  # move forward and flatten
+
     # Movements
-    moveToMiddle() # Go to middle of motor at start and end
-    rob.movej(stationJPose, acc=a,vel=v*2)
-    rob.movel(grabTapePose, acc=a,vel=v)
-#    closeGripper()
-    time.sleep(1.5) #Remove when gripper connected
-    
-    rob.movel(forwardTapePose, acc=a,vel=v)
+    moveToMiddle()  # Go to middle of motor at start and end
+    rob.movej(stationJPose, acc=a, vel=v * 2)
+    rob.movel(grabTapePose, acc=a, vel=v)
+    #    closeGripper()
+    time.sleep(1.5)  # Remove when gripper connected
+
+    rob.movel(forwardTapePose, acc=a, vel=v)
     forwardTapePose[1] -= 0.01
     rob.translate_tool((0, 0, 0.02), acc=a, vel=v)
-#    closeServo()
-    time.sleep(1.5) #Remove when servo connected
-    
-#    rob.movel(middleStatorPose, acc=a, vel=v)
-    moveToMiddle() # Go to middle of motor at start and end
+    #    closeServo()
+    time.sleep(1.5)  # Remove when servo connected
+
+    #    rob.movel(middleStatorPose, acc=a, vel=v)
+    moveToMiddle()  # Go to middle of motor at start and end
     print("Tapestation done \n")
 
 
 def moveToMiddle():
-    #Simple function to go to middle of rotor everytime
-    rob.movej(middleStatorJPose, acc=a, vel=v*2)
-    
-    
+    # Simple function to go to middle of rotor everytime
+    rob.movej(middleStatorJPose, acc=a, vel=v * 2)
+
+
 def tapeMovement():
     # Taping movement
     # TODO REMOVE SLEEPS!
-    print("Tape movement!")
-    deltaHorizontal = 0.14 #Forward distance
-    deltaVertical = 0.015 #Pushing down distance
-    ogvAngle = np.deg2rad(12) #Horizontal angle of the OGV's
-    
-    #Get correction distances from LIDAR
+    #print("Tape movement!")
+    deltaHorizontal = 0.02  # Forward distance
+    deltaVertical = 0.00  # Pushing down distance
+    ogvAngle = np.deg2rad(8)  # Horizontal angle of the OGV's
+
+    pose = rob.getl()
+
+    # Get correction distances from LIDAR
     correctionX, correctionZ = lidar.find_vane()
-    print("X = ",correctionX, "\nZ = ", correctionZ)
+    #print("X = ", correctionX, "\nZ = ", correctionZ)
     time.sleep(3)
-#    correctionX = -correctionX
-    correctionX -= 0.025 #0.0225  # offset of lidar
-    correctionZ -= (0.1825  + 0.02)  #-0.25 offset of lidar - dist EOAT + clearance
-    print("X = ",correctionX, "\nZ = ", correctionZ)
-   
+    #   correctionX = -correctionX
+    correctionX -= 0.00 #0.0225  # offset of lidar
+    correctionZ -= (0.1825 + 0.02)  # -0.25 offset of lidar - dist EOAT + clearance
+    #print("X = ", correctionX, "\nZ = ", correctionZ)
+
     rob.translate_tool((correctionX, 0, correctionZ), acc=a, vel=v)
-    
-    #Rotate the EOAT horizontal in line with the OGV's
+
+    # Rotate the EOAT horizontal in line with the OGV's
     time.sleep(3)
-    rotateDistance = math.tan(np.deg2rad(12)) * 0.258 #0.252 + distEOAT + clearance 
-    rob.translate_tool((rotateDistance, 0, 0), acc=a, vel=v) 
+    rotateDistance = math.tan(np.deg2rad(12)) * 0.258  # 0.252 + distEOAT + clearance
+    rob.translate_tool((rotateDistance, 0, 0), acc=a, vel=v)
 
     t = rob.get_pose()
     t.orient.rotate_yt(-ogvAngle)
@@ -128,42 +133,50 @@ def tapeMovement():
     time.sleep(2)
     rob.translate_tool((0, -deltaVertical, 0), acc=a, vel=v)
     rob.translate_tool((0, 0, -deltaHorizontal), acc=a, vel=v)
-    
-    #Rotate EOAT back
+
+    # Rotate EOAT back
     t = rob.get_pose()
     t.orient.rotate_yt(ogvAngle)
     rob.set_pose(t, vel=v, acc=a)
 
-    print("Tape movement done! \n")
+    rob.movel([pose[0], pose[1], pose[2], pose[3], pose[4], pose[5]], acc=a, vel=v / 2)
+
+    #print("Tape movement done! \n")
+    return correctionX, correctionZ
 
 
 if __name__ == "__main__":
-    #Configure Robot
+    # Configure Robot
     logging.basicConfig(level=logging.WARN)
     rob = urx.Robot("192.168.1.102", True)
 
+    #myrobot = MyRobot(urx.Robot("192.168.1.102", True))
+
     v = 0.1
     a = 0.3
-    diameter = 1.015 + 0.1 # + 0.1 for clearance
+    diameter = 1.015 + 0.1  # + 0.1 for clearance
     radius = diameter / 2
     invert = False
-#
-#    x, z = lidar.find_vane()
-#    print("X = ",x, "\nZ = ", z)
-    
-#    startup() #Joint move in middle of joint limits UR10
-#    calibrateCenter()  #Get center with LIDAR
-    
-#    middleStatorPose = rob.getl()
-    middleStatorPose = [-0.219 , -0.242 , 0.83 , 0.0 , -0.0 , -1.57] #remove when using LIDAR
+    #
+    #    x, z = lidar.find_vane()
+    #    print("X = ",x, "\nZ = ", z)
+
+    #    startup() #Joint move in middle of joint limits UR10
+    #    calibrateCenter()  #Get center with LIDAR
+
+    #    middleStatorPose = rob.getl()
+    middleStatorPose = [-0.219, -0.242, 0.83, 0.0, -0.0, -1.57]  # remove when using LIDAR
     middleStatorJPose = rob.getj()
-#    moveToMiddle()
-#    moveToMiddle()
+    #    moveToMiddle()
+    #    moveToMiddle()
     toolpose = rob.getl()
     print("Current Toolpose : ", toolpose)
-    
+
     try:
-        tapeMovement()
+        # startup()
+        # calibrateCenter()
+        for i in range(0, 5):
+            print( i, ": ", tapeMovement())
         '''
         i = 0 #2 # Start at 3nd OGV because woodenbeam
 #        rob.movej(middleStatorJPose, acc=a, vel=v*2)
