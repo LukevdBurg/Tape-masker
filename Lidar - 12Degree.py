97e2357bc2ce7641e26f1d085ab00e63538763bb
LukevdBurg <46902130+LukevdBurg@users.noreply.github.com>
2020-01-20 16:20:40 +0100

    Update Lidar - 12Degree.py

--- Lidar - 12Degree.py
+++ Lidar - 12Degree.py
@@ -1,7 +1,7 @@
 # Animates distances and measurment quality
-from rplidar import RPLidar
-import time
 import numpy as np
+from rplidar import RPLidar
+
 PORT_NAME = 'COM3'
 DMAX = 500  # was 4000
 IMIN = 0
@@ -9,68 +9,55 @@ IMAX = 500
 som = 0
 
 
-def run():
+def run(lower_angle_L, lower_angle_R, upper_angle_L, upper_angle_R, lower_distance_L, lower_distance_R,
+        upper_distance_L, upper_distance_R):
     mylidar = RPLidar(PORT_NAME, baudrate=115200)
-    # info = mylidar.get_info()
-    # print(info)
-    som = 0.0
     total = 0
-    aver = 0.0
-    arr_avg = []
-
-
-    # health = mylidar.get_health()
-    # print(health)
-    aver2 = 0
+    arr_avg_L = []
+    arr_avg_R = []
     mylidar_scan = []
 
-
-
-    for y in range(0,15):
+    for y in range(0, 15):
 
         for i, scan in enumerate(mylidar.iter_scans(scan_type='normal', max_buf_meas=60000)):
-            #print('%d: Got %d measures' % (i, len(scan)))
+            # print('%d: Got %d measures' % (i, len(scan)))
             #
             mylidar_scan.append(scan)
             if i > 10:
                 break
 
-        for i in range(len(mylidar_scan)): #aantal rondes
+        for i in range(len(mylidar_scan)):  # aantal rondes
 
-
-            #print("Len lidarscan i : ", len(mylidar_scan[i]))
-            for j in range(len(mylidar_scan[i])): #aantal metingen in het rondje
+            # print("Len lidarscan i : ", len(mylidar_scan[i]))
+            for j in range(len(mylidar_scan[i])):  # aantal metingen in het rondje
                 mylist = mylidar_scan[i][j]
                 # print(mylist[2])
 
-                if 86.5 < mylist[1] < 89 and 150 < mylist[2] < 300:
-                    # total = total + 1
-                    som = mylist[2]
+                if lower_angle_L < mylist[1] < upper_angle_L and lower_distance_L < mylist[2] < upper_distance_L:
+                    dist_L = mylist[2]
                     total = 1
-                    #print(mylist)
+                elif lower_angle_R < mylist[1] < upper_angle_R and lower_distance_R < mylist[2] < upper_distance_R:
+                    dist_R = mylist[2]
+                    total = 1
+
                 if total == 1:
                     # aver = som / total
-                    arr_avg.append(som)
-                    som = 0.0
+                    arr_avg_L.append(dist_L)
+                    arr_avg_R.append(dist_R)
                     total = 0
 
-            #print("arr_avg: ", arr_avg)
-            arr_mean = np.mean(arr_avg)
-            print("arr_mean: ", arr_mean)
+            # print("arr_avg: ", arr_avg)
+            arr_mean_L = np.mean(arr_avg_L)
+            arr_mean_R = np.mean(arr_avg_R)
+            print("Average Left: ", arr_mean_L)
+            print("Average Right: ", arr_mean_R)
 
-
-
-        '''
-        total2 = len(mylidar_scan)
-        aver2 = aver + aver2
-        aver2 = aver2/ total2
-        print("total aver: ",aver2)'''
         mylidar.clean_input()
-
 
     mylidar.stop()
     mylidar.stop_motor()
     mylidar.disconnect()
+    return arr_mean_L, arr_mean_R
 
 
 if __name__ == '__main__':
