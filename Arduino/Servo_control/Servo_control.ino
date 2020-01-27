139f3fae5d2217c74289143eddae7af9df705d8a
LukevdBurg <46902130+LukevdBurg@users.noreply.github.com>
2020-01-27 09:40:47 +0100

    Update Servo_control.ino

--- Arduino/Servo_control/Servo_control.ino
+++ Arduino/Servo_control/Servo_control.ino
@@ -1,25 +1,28 @@
 #include <Servo.h>
 byte inPin  = 2;
+int servoState = 0;
+int lastServoState = 0;
 
 Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
 
 void setup() {
   myservo.attach(6);  // attaches the servo on pin 9 to the servo object
   pinMode(inPin, INPUT); //Set pin 2 as input
-  myservo.write(pos); //Servo goes to default position
-  Serial.begin(9600);
+  myservo.write(180);
 }
 
 void loop() {
-bool pinVal = digitalRead(inPin);
-Serial.println(pinVal);
-if (pinVal == HIGH){
-  myservo.write(0);             // tell servo to go to cutting position
-}
-else{
-myservo.write(190);             // tell servo to go to neutral position
-}
+  servoState = digitalRead(inPin);
+  if (servoState != lastServoState) {
+    //If state has changed, send PWM to servo
+    if (servoState == HIGH){
+      //If servoState is HIGH, send to 15 degrees
+      myservo.write(15);
+    } else{
+      //If servoState is LOW, send to 190 degrees
+      myservo.write(190);
+    }
+  }
+  //Save last state
+  lastServoState = servoState;
 }
